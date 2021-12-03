55fa3fe775147095d1c566a482795da7e96c8c45
John Z <johnz@jomazu.io>
2021-12-03 05:54:59 +0000

    Update

--- sandbox/lists.exs
+++ sandbox/lists.exs
@@ -19,4 +19,24 @@ def sum([ h | t ]), do: h + sum(t)
 def square([]), do: []
 def square([ h | t ]), do: [h*h | square(t)]
 
+# Function that doubles each entry
+
+def double([]), do: []
+def double([ h | t ]), do: [h*2 | double(t)]
+
+# Use map to replace the above functions, by inserting
+# func where applicable
+
+def map([], _func), do: []
+def map([ h | t ], func), do: [ func.(h) | map(t, func) ]
+
+# Example in "iex"
+# Lists.map [1,2,3,4], fn x -> 3*x end
+# or
+# Lists.map [3,6,9], &(&1 * 5) end
+# or
+# Enum.map [1,2,3,4], fn x -> x*x*x*x end
+
+# Learn the "Enum map functions" in the Enum Module
+
 end
