65a1fe1b5d0cf86c7d56182ec98848aff64a704c
John Z <johnz@jomazu.io>
2021-12-06 04:35:36 +0000

    Update

--- hangman/hangman/test/impl_game_test.exs
+++ hangman/hangman/test/impl_game_test.exs
@@ -13,7 +13,16 @@ defmodule HangmanImplGameTest do
     game = Game.new_game("wombat")
     assert game.turns_left == 7
     assert game.game_state == :initializing
-    assert length(game.letters) == ["w", "o", "m", "b", "a", "t"]
+    assert game.letters == ["w", "o", "m", "b", "a", "t"]
+  end
+
+  test "state doesn't change if a game is won or lost" do
+    for state <- [:won, :lost] do
+      game = Game.new_game("wombat")
+      game = Map.put(game, :game_state, state)
+      {new_game, _tally} = Game.make_move(game, "x")
+      assert new_game == game
+    end
   end
 
 end
