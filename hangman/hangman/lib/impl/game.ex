7c93274c91f6044c56aecd48f5d87f719632ab69
John Z <johnz@jomazu.io>
2021-12-06 02:35:15 +0000

    Update

--- hangman/hangman/lib/impl/game.ex
+++ hangman/hangman/lib/impl/game.ex
@@ -14,10 +14,15 @@ defmodule Hangman.Impl.Game do
         used:       MapSet.new(),
     )
 
-    @spec new_game :: t
+    @spec new_game :: t 
     def new_game do
+        new_game(Dictionary.random_word)
+    end
+
+    @spec new_game :: t
+    def new_game(word) do
         %Hangman.Impl.Game{
-            letters: Dictionary.random_word |> String.codepoints
+            letters: word |> String.codepoints
         }
     end
 
