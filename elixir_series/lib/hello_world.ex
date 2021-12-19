53e3c960bd6aba7d3c5e0ca2e55c47c819080ccb
John Z <johnz@jomazu.io>
2021-12-19 03:26:50 +0000

    Update

--- elixir_series/lib/hello_world.ex
+++
@@ -1,7 +0,0 @@
-defmodule HelloWorld do
-  def hello_world do
-    IO.puts("Hello, World!")
-  end
-end
-
-HelloWorld.hello_world()
--- elixir_series/test/elixir_series_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ElixirSeriesTest do
-  use ExUnit.Case
-  doctest ElixirSeries
-
-  test "greets the world" do
-    assert ElixirSeries.hello() == :world
-  end
-end
